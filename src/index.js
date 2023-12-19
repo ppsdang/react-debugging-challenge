3b63ede40abbceb73e4843a6a7d33a9b809052b1
Pawanpreet Singh <pawanpreet.singh@oodles.io>
2023-12-19 11:51:25 +0530

    removed react dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,8 +9,8 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  <div id="doubt">
-  </div>
+  // <div id="doubt">
+  // </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
